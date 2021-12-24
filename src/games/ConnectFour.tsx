f8bf3191ca62c1a19069d44ecf31cb7b904434e8
Eric Wang <eric.wang401@gmail.com>
2021-12-24 23:13:51 +0100

    Add check winner

--- src/games/ConnectFour.tsx
+++ src/games/ConnectFour.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export interface filledPositionsInterface {
-  [index: number]: 'red' | 'yellow'
+  [index: number]: string | 'red' | 'yellow'
 }
 
-export type colors = 'yellow' | 'red'
+export type colors = string | 'yellow' | 'red'
 
 const ConnectFour = () => {
   const [filledPositions, setFilledPositions] =
     useState<filledPositionsInterface>({})
   const [turn, setTurn] = useState<colors>('yellow')
+  const [winner, setWinner] = useState<colors>('')
 
   const findPlayableSlot = (column: number) => {
     for (let i = column + 35; i >= column; i = i - 7) {
@@ -22,21 +23,113 @@ const ConnectFour = () => {
   const highlightPlayableSlotInColumn = (show: boolean, slot: number) => {
     let playableSlot = findPlayableSlot(slot % 7 === 0 ? 7 : slot % 7)
 
-
     if (show) {
       document
-        .getElementById(
-          `slot-${playableSlot}`
-        )
-        ?.classList.add((turn === 'yellow' ? 'fill-yellow-100' : 'fill-red-100'))
+        .getElementById(`slot-${playableSlot}`)
+        ?.classList.add(turn === 'yellow' ? 'fill-yellow-100' : 'fill-red-100')
     } else {
       document
-        .getElementById(
-          `slot-${playableSlot}`
+        .getElementById(`slot-${playableSlot}`)
+        ?.classList.remove(
+          turn === 'yellow' ? 'fill-yellow-100' : 'fill-red-100'
         )
-        ?.classList.remove((turn === 'yellow' ? 'fill-yellow-100' : 'fill-red-100'))
     }
   }
+
+  useEffect(() => {
+    let filledInData = { ...filledPositions }
+
+    const checkLine = (a: number, b: number, c: number, d: number): boolean => {
+      // Check first cell non-zero and all cells match
+      return a !== 0 && a === b && a === c && a === d
+    }
+
+    const checkWinner = (bd: number[][]) => {
+      // Check down
+      for (let r = 0; r < 3; r++)
+        for (let c = 0; c < 7; c++)
+          if (checkLine(bd[r][c], bd[r + 1][c], bd[r + 2][c], bd[r + 3][c]))
+            return bd[r][c]
+
+      // Check right
+      for (let r = 0; r < 6; r++)
+        for (let c = 0; c < 4; c++)
+          if (checkLine(bd[r][c], bd[r][c + 1], bd[r][c + 2], bd[r][c + 3]))
+            return bd[r][c]
+
+      // Check down-right
+      for (let r = 0; r < 3; r++)
+        for (let c = 0; c < 4; c++)
+          if (
+            checkLine(
+              bd[r][c],
+              bd[r + 1][c + 1],
+              bd[r + 2][c + 2],
+              bd[r + 3][c + 3]
+            )
+          )
+            return bd[r][c]
+
+      // Check down-left
+      for (let r = 3; r < 6; r++)
+        for (let c = 0; c < 4; c++)
+          if (
+            checkLine(
+              bd[r][c],
+              bd[r - 1][c + 1],
+              bd[r - 2][c + 2],
+              bd[r - 3][c + 3]
+            )
+          )
+            return bd[r][c]
+
+      return 0
+    }
+
+    // if there aren't moves  from 1-42, fill them in
+
+    for (let i = 1; i < 43; i++) {
+      if (filledInData[i] === undefined) {
+        filledInData[i] = ''
+      }
+    }
+
+    // convert it into an Array
+
+    let convertedData: number[][] = []
+
+    for (let i = 1; i < 42; i = i + 7) {
+      let groupedMoves: number[] = []
+
+      for (let v = 0; v <= 7; v++) {
+        let selectedMove = filledInData[i + v]
+
+        if (v === 7) {
+          convertedData.push(groupedMoves)
+        } else {
+          if (selectedMove.length === 0) {
+            groupedMoves.push(0)
+          } else if (selectedMove === 'yellow') {
+            groupedMoves.push(1)
+          } else if (selectedMove === 'red') {
+            groupedMoves.push(2)
+          }
+        }
+      }
+    }
+
+    let winner = checkWinner(convertedData)
+
+    switch (winner) {
+        case 1:
+            setWinner('yellow')
+            break;
+        case 2:
+            setWinner('red')
+            break;
+    }
+
+  }, [filledPositions])
 
   const move = (slot: number) => {
     let playableSlot = findPlayableSlot(slot % 7 === 0 ? 7 : slot % 7)
@@ -62,7 +155,11 @@ const ConnectFour = () => {
                 onMouseLeave={() => highlightPlayableSlotInColumn(false, i + 1)}
                 onClick={() => move(i + 1)}
                 viewBox='0 0 100 100'
-                className={`fill-white ${filledPositions[i + 1] === 'yellow' ? '!fill-yellow-400' : (filledPositions[i + 1] === 'red' && '!fill-red-400')}`}
+                className={`fill-white ${
+                  filledPositions[i + 1] === 'yellow'
+                    ? '!fill-yellow-400'
+                    : filledPositions[i + 1] === 'red' && '!fill-red-400'
+                }`}
                 xmlns='http://www.w3.org/2000/svg'
               >
                 <circle cx='50' cy='50' r='50' />
@@ -71,6 +168,8 @@ const ConnectFour = () => {
           )
         })}
       </div>
+
+      <div className="row-span-1 w-full h-full">{ winner }</div>
     </div>
   )
 }
